4fe9f915482ed37fa6695648abe1d9f868b1eb69
mikan3rd <mikan.no.kikoushi@gmail.com>
2018-06-07 20:47:11 +0900

    test: styleのテスト

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -4,6 +4,14 @@ import {Page, Toolbar,BackButton} from 'react-onsenui';
 
 class Browser extends React.Component {
 
+    componentDidMount = () => {
+        const innerWidth = window.innerWidth;
+        console.log("innerWidth:", innerWidth);
+        let element = document.getElementById("browser-iframe");
+        element.style.width = `${innerWidth}px`;
+        console.log("success!!");
+    }
+
     renderToolbar = () => {
         return (
             <Toolbar>
@@ -24,6 +32,7 @@ class Browser extends React.Component {
       <Page renderToolbar={this.renderToolbar}>
         <div className="c-browser__iframe__wrapper">
           <iframe
+            id="browser-iframe"
             className="c-browser__iframe"
             src={naviShop.urls.pc}
           />
